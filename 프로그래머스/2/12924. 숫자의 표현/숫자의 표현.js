c968aa9e4600e47032bcf1e4421a03647de9bfc8
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2024-10-16 13:57:51 +0900

    [level 2] Title: 숫자의 표현, Time: 2.73 ms, Memory: 36.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -0,0 +1,23 @@
+function solution(n) {
+    let answer = 0;
+    
+    for (let i = 1; i < n / 2; i++) { 
+    let sum = 0;
+    for (let j = i; j < n; j++) {
+        sum += j;
+        if (sum === n){
+            answer++;
+            break;
+        }
+        
+        if( sum > n){
+            break;
+        }
+    }
+  }
+    
+    return answer + 1;
+}
+
+
+
