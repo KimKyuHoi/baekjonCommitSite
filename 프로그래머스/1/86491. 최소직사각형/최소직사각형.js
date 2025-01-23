d9d4b171b1add3722b24a1208fa9476a15be9500
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2025-01-23 11:44:27 +0900

    [level 1] Title: 최소직사각형, Time: 6.05 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,17 +1,14 @@
 function solution(sizes) {
-    let right = 0;
     let left = 0;
+    let right = 0;
     
     sizes.map((a) => {
-        let temp = 0;
-        
-        a.sort((a,b) => a-b)
+        a.sort((a,b) => b-a)
         
         right = Math.max(right, a[0])
         left = Math.max(left, a[1])
-
     })
-
-    return left * right;
+    
+    return right * left
 }
 
