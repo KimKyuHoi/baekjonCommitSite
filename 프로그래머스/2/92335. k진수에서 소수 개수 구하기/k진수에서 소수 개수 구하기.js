2d2dd06dfc8aa5f05a2e9fb3e9edbb1c8213decb
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2025-02-17 09:15:04 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 9.63 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -0,0 +1,47 @@
+// function isPrimeNumber(number){
+//     if(number <= 1)  return false;
+    
+//     for(let i = 2; i<Math.sqrt(number); i++){
+//         if(number % i === 0)  return false;
+//     }
+    
+//     return true;
+// }
+
+
+// function solution(n, k) {
+//     var answer = 0;
+//     let numArr = n.toString(k).split('0');
+    
+//     numArr.map((a,i) => {
+//         if(isPrimeNumber(Number(numArr[i]))) answer++
+//     })
+    
+//     return answer;
+// }
+
+function isPrimeNumber(number) {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function solution(n, k) {
+  let answer = 0;
+  let num = n.toString(k); // n을 k진수로 변환
+  let numArr = num.split("0"); // 변환한 숫자를 0을 기준으로 나눠 배열에 저장
+
+  for (let i = 0; i < numArr.length; i++) {
+    if (isPrimeNumber(Number(numArr[i]))) {
+      answer++; // 해당 숫자가 소수면 answer++
+    }
+  }
+
+  return answer;
+}
