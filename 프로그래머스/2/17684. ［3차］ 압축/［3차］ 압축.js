ec061ebd6d3f3d9bd2a36001527d970a05c86d8f
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2025-03-06 12:52:50 +0900

    [level 2] Title: [3차] 압축, Time: 0.93 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.js
@@ -0,0 +1,33 @@
+function solution(msg) {
+    const dict = {};
+    for(let i = 0; i<26; i++){
+        dict[String.fromCharCode(65+i)] = i+1;
+    }
+    
+    let result = [];
+    let nextIndex = 27;
+    let pos = 0;
+    
+    while (pos < msg.length) {
+        let w = msg[pos];
+        let i = pos;
+    
+  
+        while (i + 1 < msg.length && dict[w + msg[i + 1]] !== undefined) {
+              w += msg[i + 1];
+              i++;
+        }
+    
+        result.push(dict[w]);
+
+        if (i + 1 < msg.length) {
+          const c = msg[i + 1];
+          dict[w + c] = nextIndex;
+          nextIndex++;
+        }
+
+        pos = i + 1;
+  }
+  
+  return result;
+}
