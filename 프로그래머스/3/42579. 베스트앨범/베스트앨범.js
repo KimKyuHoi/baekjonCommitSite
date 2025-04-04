db51b6e6c6a62b477310d00361fb29597b2a2eda
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2025-04-04 14:06:59 +0900

    [level 3] Title: 베스트앨범, Time: 0.60 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,46 @@
+function solution(genres, plays) {
+    const genreMap = {};
+    const songList = [];
+    
+    genres.forEach((genre, index) => {
+        if(!genreMap[genre]){
+            genreMap[genre] = 0;
+        }
+        genreMap[genre] += plays[index];
+        
+        songList.push({
+            id: index,
+            genre: genre,
+            play: plays[index]
+        });
+    })
+    
+    const sortedGenres = Object.keys(genreMap).sort((a, b) => genreMap[b] - genreMap[a]);
+    
+    const answer = [];
+    
+    sortedGenres.forEach(genre => {
+        const genreSongs = songList.filter(song => song.genre === genre);
+        
+        genreSongs.sort((a,b) => {
+            if(a.play === b.play){
+                return a.id - b.id
+            }
+            
+            return b.play - a.play;
+        })
+        
+        const topSongs = genreSongs.slice(0,2)
+        
+        topSongs.forEach(song => {
+            answer.push(song.id)
+        })
+    })
+    
+    return answer
+  
+    
+}
+
+
+
