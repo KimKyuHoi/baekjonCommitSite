5af2eecb6f43e0db88a42ba0c9a2d401514cca20
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2025-04-04 14:15:19 +0900

    [level 3] Title: 베스트앨범, Time: 0.52 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -6,31 +6,30 @@ function solution(genres, plays) {
         if(!genreMap[genre]){
             genreMap[genre] = 0;
         }
-        genreMap[genre] += plays[index];
+        genreMap[genre] += plays[index]
         
         songList.push({
             id: index,
             genre: genre,
             play: plays[index]
-        });
+        })
     })
     
-    const sortedGenres = Object.keys(genreMap).sort((a, b) => genreMap[b] - genreMap[a]);
+    const sortedGenres = Object.keys(genreMap).sort((a,b) => genreMap[b] - genreMap[a])
     
     const answer = [];
-    
     sortedGenres.forEach(genre => {
-        const genreSongs = songList.filter(song => song.genre === genre);
+        const genreSongs = songList.filter(song => song.genre === genre)
         
         genreSongs.sort((a,b) => {
             if(a.play === b.play){
                 return a.id - b.id
             }
             
-            return b.play - a.play;
+            return b.play - a.play
         })
         
-        const topSongs = genreSongs.slice(0,2)
+        const topSongs = genreSongs.slice(0,2);
         
         topSongs.forEach(song => {
             answer.push(song.id)
@@ -38,8 +37,6 @@ function solution(genres, plays) {
     })
     
     return answer
-  
-    
 }
 
 
