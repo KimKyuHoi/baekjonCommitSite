3231b21880ddf33a8e0ab60baa2de4294901dde0
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2025-05-18 04:14:46 +0900

    [level 2] Title: 프로세스, Time: 385.00 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,28 +1,23 @@
 function solution(priorities, location) {
-     let queue = priorities.map((priority, index) => ({
-        priority: priority,
-        index: index
-    }));
-    
+     let queue = priorities.map((a, i) => ({
+         priority: a,
+         index: i
+     }))
+     
+     let order = 0;
     console.log(queue)
-    let order = 0;
     
-    while(queue.length > 0) {
-        // 1. 첫 번째 프로세스를 꺼냅니다.
+    while(queue.length > 0){
         let current = queue.shift();
+        console.log('current', current)
         
-        // 2. 남은 프로세스 중 더 높은 우선순위가 있는지 확인
-        let hasHigherPriority = queue.some(process => process.priority > current.priority);
-        
-        if(hasHigherPriority) {
-            // 더 높은 우선순위가 있다면 현재 프로세스를 큐의 끝에 추가
+        let hasHigherPriority = queue.some(process => process.priority > current.priority)
+        console.log('higherPriority', hasHigherPriority)
+        if(hasHigherPriority){
             queue.push(current);
-        } else {
-            // 더 높은 우선순위가 없다면 현재 프로세스 실행
+        }else{
             order++;
-            
-            // 찾고자 하는 프로세스라면 순서를 반환
-            if(current.index === location) {
+            if(current.index === location){
                 return order;
             }
         }
