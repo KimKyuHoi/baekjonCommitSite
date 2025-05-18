fabf674278c09225d19248755c3ef269060e5ae6
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2025-05-18 12:27:11 +0900

    [level 2] Title: 더 맵게, Time: 652.36 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.js
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -5,40 +5,15 @@ class MinHeap {
 
   push(val) {
     this.heap.push(val);
-    let i = this.heap.length - 1;
-    while (i > 0) {
-      const parent = Math.floor((i - 1) / 2);
-      if (this.heap[parent] <= this.heap[i]) break;
-      [this.heap[i], this.heap[parent]] = [this.heap[parent], this.heap[i]];
-      i = parent;
-    }
+    this._bubbleUp();
   }
 
   pop() {
     if (this.heap.length === 1) return this.heap.pop();
-
-    const min = this.heap[0];
+    const top = this.heap[0];
     this.heap[0] = this.heap.pop();
-    let i = 0;
-
-    while (true) {
-      const left = i * 2 + 1;
-      const right = i * 2 + 2;
-      let smallest = i;
-
-      if (left < this.heap.length && this.heap[left] < this.heap[smallest]) {
-        smallest = left;
-      }
-      if (right < this.heap.length && this.heap[right] < this.heap[smallest]) {
-        smallest = right;
-      }
-
-      if (smallest === i) break;
-      [this.heap[i], this.heap[smallest]] = [this.heap[smallest], this.heap[i]];
-      i = smallest;
-    }
-
-    return min;
+    this._bubbleDown();
+    return top;
   }
 
   peek() {
@@ -48,23 +23,63 @@ class MinHeap {
   size() {
     return this.heap.length;
   }
+
+  _bubbleUp() {
+    let idx = this.heap.length - 1;
+    const element = this.heap[idx];
+    while (idx > 0) {
+      const parentIdx = Math.floor((idx - 1) / 2);
+      const parent = this.heap[parentIdx];
+      if (element >= parent) break;
+      this.heap[idx] = parent;
+      idx = parentIdx;
+    }
+    this.heap[idx] = element;
+  }
+
+  _bubbleDown() {
+    let idx = 0;
+    const length = this.heap.length;
+    const element = this.heap[0];
+
+    while (true) {
+      let left = 2 * idx + 1;
+      let right = 2 * idx + 2;
+      let smallest = idx;
+
+      if (left < length && this.heap[left] < this.heap[smallest]) {
+        smallest = left;
+      }
+
+      if (right < length && this.heap[right] < this.heap[smallest]) {
+        smallest = right;
+      }
+
+      if (smallest === idx) break;
+
+      this.heap[idx] = this.heap[smallest];
+      idx = smallest;
+    }
+
+    this.heap[idx] = element;
+  }
 }
 
-
-
 function solution(scoville, K) {
-  const heap = new MinHeap();
-
-  scoville.forEach(s => heap.push(s));
+  const minHeap = new MinHeap();
+  for (const s of scoville) {
+    minHeap.push(s);
+  }
 
   let count = 0;
 
-  while (heap.size() > 1 && heap.peek() < K) {
-    const a = heap.pop();
-    const b = heap.pop();
-    heap.push(a + b * 2);
+  while (minHeap.size() >= 2 && minHeap.peek() < K) {
+    const first = minHeap.pop();
+    const second = minHeap.pop();
+    const mixed = first + second * 2;
+    minHeap.push(mixed);
     count++;
   }
 
-  return heap.peek() >= K ? count : -1;
+  return minHeap.peek() >= K ? count : -1;
 }
