1edf930e753bbb13fd00220ec6687ede1b8ce5fa
김규회 <48755156+KimKyuHoi@users.noreply.github.com>
2025-05-18 12:32:09 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.38 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -1,31 +1,90 @@
-function solution(jobs) {
-    jobs.sort((a,b) => a[0] - b[0])
-    let time = 0;
-  let total = 0;
-  let i = 0;
-  let count = 0;
-  const waiting = [];
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
 
-  while (count < jobs.length) {
-    // 현재 시간에 들어온 작업 추가
-    while (i < jobs.length && jobs[i][0] <= time) {
-      waiting.push(jobs[i++]);
+  push(val) {
+    this.heap.push(val);
+    this._bubbleUp();
+  }
+
+  pop() {
+    if (this.heap.length === 1) return this.heap.pop();
+    const min = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this._bubbleDown();
+    return min;
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  _bubbleUp() {
+    let i = this.heap.length - 1;
+    while (i > 0) {
+      const parent = Math.floor((i - 1) / 2);
+      if (this.heap[parent][0] <= this.heap[i][0]) break; // 작업시간 비교
+      [this.heap[i], this.heap[parent]] = [this.heap[parent], this.heap[i]];
+      i = parent;
+    }
+  }
+
+  _bubbleDown() {
+    let i = 0;
+    const length = this.heap.length;
+    while (true) {
+      const left = 2 * i + 1;
+      const right = 2 * i + 2;
+      let smallest = i;
+
+      if (left < length && this.heap[left][0] < this.heap[smallest][0]) {
+        smallest = left;
+      }
+      if (right < length && this.heap[right][0] < this.heap[smallest][0]) {
+        smallest = right;
+      }
+      if (smallest === i) break;
+      [this.heap[i], this.heap[smallest]] = [this.heap[smallest], this.heap[i]];
+      i = smallest;
+    }
+  }
+}
+
+    
+
+
+
+function solution(jobs) {
+  const heap = new MinHeap();
+  jobs.sort((a, b) => a[0] - b[0]); // 요청 시간 기준 정렬
+
+  let time = 0;     // 현재 시간
+  let i = 0;        // jobs 인덱스
+  let total = 0;    // 총 소요 시간
+  const n = jobs.length;
+
+  while (i < n || heap.size() > 0) {
+    // 현재 시점까지 요청된 작업을 힙에 넣기
+    while (i < n && jobs[i][0] <= time) {
+      const [start, duration] = jobs[i];
+      heap.push([duration, start]); // 작업시간 기준으로 넣기
+      i++;
     }
 
-    // 처리 가능한 작업이 있다면
-    if (waiting.length > 0) {
-      // 소요 시간 기준으로 정렬
-      waiting.sort((a, b) => a[1] - b[1]);
-
-      const [start, duration] = waiting.shift();
+    if (heap.size() > 0) {
+      const [duration, start] = heap.pop();
       time += duration;
       total += time - start;
-      count++;
     } else {
-      // 다음 작업 시간까지 건너뜀
+      // 대기 중인 작업이 없으면 다음 작업 시간으로 점프
       time = jobs[i][0];
     }
   }
 
-  return Math.floor(total / jobs.length);
+  return Math.floor(total / n);
 }
